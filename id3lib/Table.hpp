9c545a43010c6e07620d16452609784ff77e1d40
Marek Jasiński <91marek@gmail.com>
2012-04-12 20:41:59 +0200

    Komentarze w klasie Table

--- id3lib/Table.hpp
+++ id3lib/Table.hpp
@@ -17,34 +17,58 @@ namespace id3lib {
  */
 class Table {
 public:
+	/*
+	 * Tworzy pusta tabele o okreslonych atrybutach
+	 */
 	Table(const std::vector<std::string>& attr) :
 			attr_(attr), examples_() , columns_(attr.size()) {
 	}
-
+	/*
+	 * Dodaje przyklad na koniec kolekcji
+	 */
 	void pushBack(const std::vector<std::string>& x) throw(BadSizeException);
-
+	/*
+	 * @return Nazwa atrybutu o podanym indeksie
+	 */
 	const std::string& getAttrAt(size_t i) const {
 		return attr_.at(i);
 	}
-
+	/*
+	 * @return Kolekcja nazw atrybutow
+	 */
 	const std::vector<std::string>& getAttr() const {
 		return attr_;
 	}
-
+	/*
+	 * @return Wartosc okreslonego atrybutu okreslonego przykladu
+	 */
 	const std::string& getValueAt(size_t x, size_t y) const {
 		return examples_[x][y];
 	}
-
+	/*
+	 * @return Liczba kolumn
+	 */
 	size_t columns() const {
 		return columns_;
 	}
-
+	/*
+	 * @return Liczba wierszy
+	 */
 	size_t rows() const {
 		return examples_.size();
 	}
 protected:
+	/*
+	 * Nazwy atrybutow
+	 */
 	const std::vector<std::string> attr_;
+	/*
+	 * Przyklady
+	 */
 	std::vector<std::vector<std::string> > examples_;
+	/*
+	 * Liczba kolumn
+	 */
 	const size_t columns_;
 };
 
