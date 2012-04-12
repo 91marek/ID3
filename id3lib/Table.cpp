c8a3c61956b14cfb7bdde3eb1b155923c31195f0
Marek Jasiński <91marek@gmail.com>
2012-04-12 13:33:23 +0200

    Finalna implementacja klasy Table

--- id3lib/Table.cpp
+++ id3lib/Table.cpp
@@ -3,35 +3,13 @@
  */
 
 #include "Table.hpp"
+#include "BadSizeException.hpp"
 
 using namespace std;
-
-Table::Table(const vector<string>& attr) :
-		attr_(attr), examples_() {
-}
-
-Table::Table(const vector<string>& attr, unsigned rows) :
-		attr_(attr), examples_(rows) {
-}
-
-Table::~Table() {
-
-}
+using namespace id3lib;
 
 void Table::pushBack(const vector<string>& x) {
 	if (x.size() != columns())
-		throw exception();
+		throw BadSizeException();
 	examples_.push_back(x);
-}
-
-vector<string> Table::getAttr() const {
-	return attr_;
-}
-
-unsigned Table::columns() const {
-	return attr_.size();
-}
-
-unsigned Table::rows() const {
-	return examples_.size();
 }
--- id3lib/Table.hpp
+++ id3lib/Table.hpp
@@ -9,6 +9,7 @@
 #include <vector>
 #include <exception>
 
+namespace id3lib {
 /*
  * Klasa przechowujaca przyklady. Atrybuty sa inicjowane
  * w konstruktorze i sa stale dla obiektu.
@@ -17,16 +18,34 @@ class Table {
 private:
 	const std::vector<std::string> attr_;
 	std::vector<std::vector<std::string> > examples_;
+	const size_t columns_;
 public:
-
-	Table(const std::vector<std::string>& attr);
-	Table(const std::vector<std::string>& attr, unsigned rows);
-	~Table();
+	Table(const std::vector<std::string>& attr) :
+			attr_(attr), examples_() , columns_(attr.size()) {
+	}
 
 	void pushBack(const std::vector<std::string>& x);
-	std::vector<std::string> getAttr() const;
-	unsigned columns() const;
-	unsigned rows() const;
+
+	const std::string& getAttrAt(size_t i) const {
+		return attr_.at(i);
+	}
+
+	const std::vector<std::string>& getAttr() const {
+		return attr_;
+	}
+
+	const std::string& getValueAt(size_t x, size_t y) const {
+		return examples_[x][y];
+	}
+
+	size_t columns() const {
+		return columns_;
+	}
+
+	size_t rows() const {
+		return examples_.size();
+	}
 };
+} /* namespace id3lib */
 
 #endif /* TABLE_HPP_ */
