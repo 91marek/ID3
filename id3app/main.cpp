9f35302ed949a35438efbdf841cfd6f2c82a3940
Marek Jasinski <91marek@gmail.com>
2012-05-30 16:40:35 +0200

    id3app

--- id3app/main.cpp
+++ id3app/main.cpp
@@ -8,150 +8,130 @@
 #include <id3lib/ZPRDecisionTree.hpp>
 #include <id3lib/UMDecisionTree.hpp>
 #include <stdexcept>
+#include <boost/tokenizer.hpp>
 
 using namespace std;
 using namespace boost;
 using namespace id3lib;
 
-int main() {
-	cout << "Hello ID3!" << endl;
-
-	/* ---ENJOY_SPORT--- */
-	vector<string> attributes = vector<string>();
-	attributes.push_back("outlook");
-	attributes.push_back("temperature");
-	attributes.push_back("humidity");
-	attributes.push_back("wind");
-	attributes.push_back("enjoy_sport");
-	TrainingSet examples = TrainingSet(attributes, 4, "?");
-	vector<string> ex0 = vector<string>();
-	ex0.push_back("sunny");
-	ex0.push_back("?");
-//	ex0.push_back("hot");
-	ex0.push_back("high");
-	ex0.push_back("normal");
-	ex0.push_back("no");
-	vector<string> ex1 = vector<string>();
-	ex1.push_back("sunny");
-	ex1.push_back("?");
-//	ex1.push_back("hot");
-	ex1.push_back("high");
-	ex1.push_back("high");
-	ex1.push_back("no");
-	vector<string> ex2 = vector<string>();
-	ex2.push_back("overcast");
-	ex2.push_back("?");
-//	ex2.push_back("hot");
-	ex2.push_back("high");
-	ex2.push_back("normal");
-	ex2.push_back("yes");
-	vector<string> ex3 = vector<string>();
-	ex3.push_back("rainy");
-	ex3.push_back("?");
-//	ex3.push_back("mild");
-	ex3.push_back("high");
-	ex3.push_back("normal");
-	ex3.push_back("yes");
-	vector<string> ex4 = vector<string>();
-	ex4.push_back("rainy");
-	ex4.push_back("?");
-//	ex4.push_back("cold");
-	ex4.push_back("normal");
-	ex4.push_back("normal");
-	ex4.push_back("yes");
-	vector<string> ex5 = vector<string>();
-	ex5.push_back("rainy");
-	ex5.push_back("?");
-//	ex5.push_back("cold");
-	ex5.push_back("normal");
-	ex5.push_back("high");
-	ex5.push_back("no");
-	examples.pushBack(ex0);
-	examples.pushBack(ex1);
-	examples.pushBack(ex2);
-	examples.pushBack(ex3);
-	examples.pushBack(ex4);
-	examples.pushBack(ex5);
-	cout << "examples:" << endl;
-	for (size_t i = 0; i < examples.rows(); ++i) {
-		for (size_t j = 0; j < examples.columns(); ++j) {
-			cout << examples.getValueAt(i, j) << " ";
-		}
-		cout << endl;
+int main(int argc, char* argv[]) {
+	if (argc != 4) {
+		cout
+				<< "Usage: building_set_file prunning_set_file to_classify_set_file"
+				<< endl;
 	}
-	UMDecisionTree dt = UMDecisionTree();
-	dt.build(examples);
-	dt.minimumErrorPruning(dt.getCategoryCount());
-	shared_ptr<vector<string> > categories(dt.classify(examples));
-	ErrorRate err = ErrorRate();
-	cout << "Wynik klasyfikacji:" << endl;
-	for (size_t i = 0; i < categories->size(); ++i) {
-		string before = examples.getValueAt(i, 4);
-		string after = (*categories)[i];
-		cout << "pierwotnie: " << before << " wynik: " << after;
-		if (!(before == after)) {
-			cout << " INACZEJ";
-			err.misclassifiedExample();
-		}
-		else {
-			cout << " TAK SAMO";
-			err.correctlyClassifiedExample();
-		}
-		cout << endl;
-	}
-	cout << "Error rate: " << err.get() << endl;
-	dt.reducedErrorPruning(examples);
 
-	/* ---ENJOY_SPORT--- */
-	TrainingSet e = TrainingSet(attributes, 4, "?");
+	// domyslne wartosci
+	char separator[] = " ,";
+	char missing_mark[] = "?";
+	unsigned category_index = 0;
+
+	/*
+	 UMDecisionTree dt = UMDecisionTree();
+	 dt.build(examples);
+	 dt.minimumErrorPruning(dt.getCategoryCount());
+	 shared_ptr<vector<string> > categories(dt.classify(examples));
+	 ErrorRate err = ErrorRate();
+	 cout << "Wynik klasyfikacji:" << endl;
+	 for (size_t i = 0; i < categories->size(); ++i) {
+	 string before = examples.getValueAt(i, 4);
+	 string after = (*categories)[i];
+	 cout << "pierwotnie: " << before << " wynik: " << after;
+	 if (!(before == after)) {
+	 cout << " INACZEJ";
+	 err.misclassifiedExample();
+	 } else {
+	 cout << " TAK SAMO";
+	 err.correctlyClassifiedExample();
+	 }
+	 cout << endl;
+	 }
+	 cout << "Error rate: " << err.get() << endl;
+	 dt.reducedErrorPruning(examples);
+	 */
+
 	try {
-		ifstream file("enjoy_sport.txt");
-		e.readFromStream(file, ", ");
-		ZPRDecisionTree dt3 = ZPRDecisionTree();
-		dt3.build(e);
-	} catch (const std::exception& e) {
-		cerr << e.what() << endl;
-	}
+		/* Building tree */
+		vector<string> attr = vector<string>();
+		string line = "";
+		ifstream building_set_file(argv[1]);
+		if (building_set_file.is_open()) {
+			getline(building_set_file, line);
+		} else
+			cout << "Unable to open file: " << argv[1] << endl;
 
-	/* ---EAT_MUSHROOM--- */
-	vector<string> attr = vector<string>();
-	attr.push_back("eat");
-	attr.push_back("cap-shape");
-	attr.push_back("cap-surface");
-	attr.push_back("cap-color");
-	attr.push_back("bruises");
-	attr.push_back("odor");
-	attr.push_back("gill-attachment");
-	attr.push_back("gill-spacing");
-	attr.push_back("gill-size");
-	attr.push_back("gill-color");
-	attr.push_back("stalk-shape");
-	attr.push_back("stalk-root");
-	attr.push_back("stalk-surface-above-ring");
-	attr.push_back("stalk-surface-below-ring");
-	attr.push_back("stalk-color-above-ring");
-	attr.push_back("stalk-color-below-ring");
-	attr.push_back("veil-type");
-	attr.push_back("veil-color");
-	attr.push_back("ring-number");
-	attr.push_back("ring-type");
-	attr.push_back("spore-print-color");
-	attr.push_back("population");
-	attr.push_back("habitat");
-	TrainingSet mushroom_table = TrainingSet(attr, 0, "?");
-	try {
-		ifstream file2("eat_mushroom.txt");
-		size_t examplesCount = mushroom_table.readFromStream(file2, ", ");
-		ZPRDecisionTree mushroom_dt = ZPRDecisionTree();
-		mushroom_dt.build(mushroom_table);
-		shared_ptr<vector<string> > result(mushroom_dt.classify(mushroom_table));
+		typedef tokenizer<char_separator<char> > tokenizer;
+		char_separator<char> sep(separator);
+		tokenizer tok(line, sep);
+		for (tokenizer::iterator tok_iter = tok.begin(); tok_iter != tok.end();
+				++tok_iter)
+			attr.push_back(string(*tok_iter));
+
+		TrainingSet building_table = TrainingSet(attr, category_index,
+				missing_mark);
+		size_t examplesCount = building_table.readFromStream(building_set_file,
+				separator);
+		cout << "Liczba wczytanych przykladow do budowy drzewa: " << examplesCount << endl;
+		building_set_file.close();
+		ZPRDecisionTree dt = ZPRDecisionTree();
+		dt.build(building_table);
+		cout << "Zbudowane drzewo:" << endl;
+		cout << dt;
+
+		/* Classifying */
+		attr = vector<string>();
+		line = "";
+		ifstream to_classify_set_file(argv[3]);
+		if (to_classify_set_file.is_open()) {
+			getline(to_classify_set_file, line);
+		} else
+			cout << "Unable to open file: " << argv[3] << endl;
+
+		tok = tokenizer(line, sep);
+		for (tokenizer::iterator tok_iter = tok.begin(); tok_iter != tok.end();
+				++tok_iter)
+			attr.push_back(string(*tok_iter));
+
+		TrainingSet to_classify_table = TrainingSet(attr, category_index, missing_mark);
+		examplesCount = to_classify_table.readFromStream(to_classify_set_file, separator);
+		cout << "Liczba wczytanych przykladow do klasyfikacji: " << examplesCount << endl;
+		to_classify_set_file.close();
+		shared_ptr<vector<string> > result(dt.classify(to_classify_table));
 		ErrorRate er = ErrorRate();
-		er.count(result, mushroom_table);
-		cout << "Liczba wczytanych przykladow: " << examplesCount << " Error rate: " << er.get() << endl;
+		er.count(result, to_classify_table);
+		cout << "Error rate: " << er.get() << endl;
+
+		/* Prunning tree */
+		attr = vector<string>();
+		line = "";
+		ifstream prunning_set_file(argv[2]);
+		if (prunning_set_file.is_open()) {
+			getline(prunning_set_file, line);
+		} else
+			cout << "Unable to open file: " << argv[2] << endl;
+
+		tok = tokenizer(line, sep);
+		for (tokenizer::iterator tok_iter = tok.begin(); tok_iter != tok.end();
+				++tok_iter)
+			attr.push_back(string(*tok_iter));
+
+		TrainingSet prunning_table = TrainingSet(attr, category_index,
+				missing_mark);
+		examplesCount = prunning_table.readFromStream(prunning_set_file,
+				separator);
+		cout << "Liczba wczytanych przykladow do przyciecia drzewa: " << examplesCount << endl;
+		prunning_set_file.close();
+		dt.reducedErrorPruning(prunning_table);
+		cout << "Przyciete drzewo:" << endl;
+		cout << dt;
+
+		shared_ptr<vector<string> > result2(dt.classify(to_classify_table));
+		ErrorRate er2 = ErrorRate();
+		er2.count(result2, to_classify_table);
+		cout << "Error rate: " << er2.get() << endl;
 	} catch (std::exception& e) {
 		cerr << e.what() << endl;
 	}
 
-	cout << "Good Bye ID3!" << endl;
 	return 0;
 }
