fef0e2136707b4f5b9998b2d8517de379daab723
Marek Jasinski <91marek@gmail.com>
2012-06-01 00:46:06 +0200

    Dwa tryby dzialania aplikacji: ZPR i UM

--- id3app/main.cpp
+++ id3app/main.cpp
@@ -3,6 +3,7 @@
  */
 
 #include <iostream>
+#include <cstdlib>
 #include <fstream>
 #include <id3lib/DecisionTree.hpp>
 #include <id3lib/ZPRDecisionTree.hpp>
@@ -27,18 +28,43 @@ using std::cerr;
 using std::ifstream;
 using namespace id3lib;
 
+const int ZPR_MODE = 0;
+const int UM_MODE = 1;
+
 int main(int argc, char* argv[]) {
-	if (argc != 4) {
-		cout
-				<< "Usage: building_set_file prunning_set_file to_classify_set_file"
+	if (argc != 5 && argc != 6) {
+		cout << "ZPR usage:" << endl
+				<< "building_set_file prunning_set_file to_classify_set_file category_index"
+				<< endl << "UM usage:" << endl
+				<< "building_set_file prunning_set_file to_classify_set_file category_index mep_parameter"
 				<< endl;
 		return EXIT_FAILURE;
+	}
+
+	int readed_category_index = atoi(argv[4]);
+	if (readed_category_index < 0) {
+		cerr << "category_index must be > 0" << endl;
+		return EXIT_FAILURE;
+	}
+	unsigned category_index = static_cast<unsigned>(readed_category_index);
+
+	int mode = ZPR_MODE;
+
+	// UM mode
+	unsigned mep_parameter = 0;
+	if (6 == argc) {
+		mode = UM_MODE;
+		int readed_mep_parameter = atoi(argv[5]);
+		if (readed_mep_parameter < 0) {
+			cerr << "mep_parameter must be > 0" << endl;
+			return EXIT_FAILURE;
+		}
+		mep_parameter = static_cast<unsigned>(readed_mep_parameter);
 	}
 
 	// domyslne wartosci
 	char separator[] = " ,";
 	char missing_mark[] = "?";
-	unsigned category_index = 0;
 
 	try {
 		/* Building tree */
@@ -61,12 +87,17 @@ int main(int argc, char* argv[]) {
 				missing_mark);
 		size_t examplesCount = building_table.readFromStream(building_set_file,
 				separator);
-		cout << "Liczba wczytanych przykladow do budowy drzewa: " << examplesCount << endl;
+		cout << "Liczba wczytanych przykladow do budowy drzewa: "
+				<< examplesCount << endl;
 		building_set_file.close();
-		ZPRDecisionTree dt = ZPRDecisionTree();
-		dt.build(building_table);
+		DecisionTree* dt = NULL;
+		if (ZPR_MODE == mode)
+			dt = new ZPRDecisionTree();
+		else
+			dt = new UMDecisionTree();
+		dt->build(building_table);
 		cout << "Zbudowane drzewo:" << endl;
-		cout << dt;
+		cout << *dt;
 
 		/* Classifying */
 		attr = vector<string>();
@@ -82,11 +113,14 @@ int main(int argc, char* argv[]) {
 				++tok_iter)
 			attr.push_back(string(*tok_iter));
 
-		TrainingSet to_classify_table = TrainingSet(attr, category_index, missing_mark);
-		examplesCount = to_classify_table.readFromStream(to_classify_set_file, separator);
-		cout << "Liczba wczytanych przykladow do klasyfikacji: " << examplesCount << endl;
+		TrainingSet to_classify_table = TrainingSet(attr, category_index,
+				missing_mark);
+		examplesCount = to_classify_table.readFromStream(to_classify_set_file,
+				separator);
+		cout << "Liczba wczytanych przykladow do klasyfikacji: "
+				<< examplesCount << endl;
 		to_classify_set_file.close();
-		shared_ptr<vector<string> > result(dt.classify(to_classify_table));
+		shared_ptr<vector<string> > result(dt->classify(to_classify_table));
 		ErrorRate er = ErrorRate();
 		er.count(result, to_classify_table);
 		cout << "Error rate: " << er.get() << endl;
@@ -109,16 +143,21 @@ int main(int argc, char* argv[]) {
 				missing_mark);
 		examplesCount = prunning_table.readFromStream(prunning_set_file,
 				separator);
-		cout << "Liczba wczytanych przykladow do przyciecia drzewa: " << examplesCount << endl;
+		cout << "Liczba wczytanych przykladow do przyciecia drzewa: "
+				<< examplesCount << endl;
 		prunning_set_file.close();
-		dt.reducedErrorPruning(prunning_table);
+		if (ZPR_MODE == mode)
+			static_cast<ZPRDecisionTree*>(dt)->reducedErrorPruning(prunning_table);
+		else
+			static_cast<UMDecisionTree*>(dt)->minimumErrorPruning(mep_parameter);
 		cout << "Przyciete drzewo:" << endl;
-		cout << dt;
+		cout << *dt;
 
-		shared_ptr<vector<string> > result2(dt.classify(to_classify_table));
+		shared_ptr<vector<string> > result2(dt->classify(to_classify_table));
 		ErrorRate er2 = ErrorRate();
 		er2.count(result2, to_classify_table);
 		cout << "Error rate: " << er2.get() << endl;
+		delete dt;
 	} catch (std::exception& e) {
 		cerr << e.what() << endl;
 	}
